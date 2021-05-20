312851d2f052bc520cb748fa183eeb3a2b8140bc
Adamor Henner <adacandido9@gmail.com>
2021-05-20 19:56:45 -0300

    Transformando a arvore em tipo generico

--- aed-ii-tree-api/br/edu/uni7/tree/BinaryTree.java
+++ aed-ii-tree-api/br/edu/uni7/tree/BinaryTree.java
@@ -1,32 +1,31 @@
 package br.edu.uni7.tree;
 
-public class BinaryTree {
-	private String key;
-	private BinaryTree leftChild;
-	private BinaryTree rightChild;
+public class BinaryTree <T> {
+	private T key;
+	private BinaryTree<T> leftChild;
+	private BinaryTree<T> rightChild;
 	
-	public BinaryTree(String rootObj) {
+	public BinaryTree(T rootObj) {
 		this.key = rootObj;
 		this.leftChild = null;
 		this.rightChild = null;
-		
 	}
 	
-	public void insertLeft(String newNode) {
+	public void insertLeft(T newNode) {
 		if (this.leftChild == null) {
-			this.leftChild = new BinaryTree(newNode);
+			this.leftChild = new BinaryTree<T>(newNode);
 		} else {
-			BinaryTree t = new BinaryTree(newNode);
+			BinaryTree<T> t = new BinaryTree<T>(newNode);
 			t.leftChild = this.leftChild;
 			this.leftChild = t;
 		}
 		
 	}
-	public void insertRight(String newNode) {
+	public void insertRight(T newNode) {
 		if (this.rightChild == null) {
-			this.rightChild = new BinaryTree(newNode);
+			this.rightChild = new BinaryTree<T>(newNode);
 		} else {
-			BinaryTree t = new BinaryTree(newNode);
+			BinaryTree<T> t = new BinaryTree<T>(newNode);
 			t.rightChild = this.rightChild;
 			this.rightChild = t;
 		}
@@ -34,19 +33,19 @@ public class BinaryTree {
 	}
 	
 	
-	public BinaryTree getLeftChild() {
+	public BinaryTree<T> getLeftChild() {
 		return leftChild;
 	}
 	
-	public BinaryTree getRightChild() {
+	public BinaryTree<T> getRightChild() {
 		return rightChild;
 	}
 	
-	public String getKey() {
+	public T getKey() {
 		return key;
 	}
 	
-	public void setKey(String key) {
+	public void setKey(T key) {
 		this.key = key;
 	}
 	 
--- aed-ii-tree-api/br/edu/uni7/tree/util/TesteBinaryTree.java
+++ aed-ii-tree-api/br/edu/uni7/tree/util/TesteBinaryTree.java
@@ -3,17 +3,21 @@ package br.edu.uni7.tree.util;
 import br.edu.uni7.tree.BinaryTree;
 
 public class TesteBinaryTree {
-
 	public static void main(String[] args) {
-		BinaryTree r = new BinaryTree("a");
+		BinaryTree<String> r = new BinaryTree<String>("a");
+		BinaryTree<Integer>b2 = new BinaryTree<Integer>(10);
+
 		r.insertLeft("b");
-		r.insertRight("right");
+		r.insertRight("c");
 		r.getLeftChild().insertLeft("k");
 		
-		System.out.println(r.getKey());
+		b2.insertLeft(20);
+		b2.insertRight(30);
 		
-		System.out.println(r.getLeftChild().getLeftChild().getKey());
+		System.out.println(b2);
 		
-		System.out.println(r);
+		
+
+		
 	}
 }

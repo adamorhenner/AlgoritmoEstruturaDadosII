8b276ad38ce263072b067c5ecaa7e2bfbbe4115f
Adamor Henner <adacandido9@gmail.com>
2021-06-01 20:17:33 -0300

    Testando deletar menor

--- aed-ii-tree-api/br/edu/uni7/tree/util/TesteBinHeap.java
+++ aed-ii-tree-api/br/edu/uni7/tree/util/TesteBinHeap.java
@@ -5,16 +5,26 @@ import br.edu.uni7.tree.BinHeap;
 public class TesteBinHeap {
 	public static void main(String[] args) {
 		BinHeap bh = new BinHeap();
-		bh.insert(5);
-		bh.insert(7);
-		bh.insert(3);
-		bh.insert(11);
-		bh.insert(2);
-		bh.insert(10);
-		bh.insert(1);
 		
-		System.out.println(bh);
+		// 1..50 - idoso e gestantes
+		//51..100 - Atendimento normal
+		
+		bh.insert(51);
+		bh.insert(52);
+		bh.insert(53);
+		bh.insert(1);
+		bh.insert(54);
+		bh.insert(2);
+		bh.insert(3);
+		bh.insert(55);
+		bh.insert(4);
+		bh.insert(56);
+		bh.insert(57);
 
+		while(!bh.isEmpty()) {
+			Integer min = bh.delMin();
+			System.out.println("Min: " + min);
+		}
 
 	}
 }

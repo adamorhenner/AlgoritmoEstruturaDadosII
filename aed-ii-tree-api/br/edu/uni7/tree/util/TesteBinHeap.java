bf1df7db45ab1aae169e7d684c3401bdc1dca367
Adamor Henner <adacandido9@gmail.com>
2021-06-08 21:18:10 -0300

    Testando Build Heap

--- aed-ii-tree-api/br/edu/uni7/tree/util/TesteBinHeap.java
+++ aed-ii-tree-api/br/edu/uni7/tree/util/TesteBinHeap.java
@@ -1,5 +1,7 @@
 package br.edu.uni7.tree.util;
 
+import java.util.Arrays;
+
 import br.edu.uni7.tree.BinHeap;
 import sun.nio.cs.ext.Big5_HKSCS_2001;
 
@@ -11,19 +13,31 @@ public class TesteBinHeap {
 		// 1..50 - idoso e gestantes
 		//51..100 - Atendimento normal
 		
-		bh.insert(2);
-		bh.insert(1);
-		bh.insert(4);
-		bh.insert(3);
-
+		Integer[] prioridade = { 2, 1, 3, 4, 5};
+		Arrays.sort(prioridade);
+		bh.buildHeap(prioridade);
 		
-		bh2.insert(51);
-		bh2.insert(52);
-		bh2.insert(53);
-		bh2.insert(54);
-		bh2.insert(55);
-		bh2.insert(56);
-		bh2.insert(57);
+		
+		
+//		for (Integer n: prioridade) {
+//			bh.insert(n);
+//		}
+//		
+//		bh.insert(2);
+//		bh.insert(1);
+//		bh.insert(4);
+//		bh.insert(3);
+
+		Integer np[] = {51,52,53,54,55,56,57};
+		bh2.buildHeap(np);
+		
+//		bh2.insert(51);
+//		bh2.insert(52);
+//		bh2.insert(53);
+//		bh2.insert(54);
+//		bh2.insert(55);
+//		bh2.insert(56);
+//		bh2.insert(57);
 
 		int priori = 0;
 		int normal = 0;
